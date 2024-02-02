437f126c03f1601ebc63ea9dc8548cc5e5fe0809
Kankawee Ramsri <email kankaveeramsri22@gmail.com>
2024-02-02 19:54:00 +0700

    add main.css file

--- flaskblog.py
+++ flaskblog.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Kankavee Ramri',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Peerada Anusakchaikul',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
